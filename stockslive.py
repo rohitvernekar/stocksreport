71cf965e56809b4a642d7e3264900cef3f7ae867
abhijit <abi.vernekar>
2017-10-29 22:23:36 +0530

    Code to calculate total of prices, brought prices , low and high

--- stockslive.py
+++ stockslive.py
@@ -5,20 +5,18 @@ from utils import csv_to_dict
 
 class StockReader(object):
     def __init__(self):
-    	self.nse = Nse()
-    	self.stock_details = csv_to_dict()
-
+        self.nse = Nse()
+        self.stock_details = csv_to_dict()
 
     def get_stock_quote(self, stock_code):
     	return self.nse.get_quote(stock_code)
 
 
     def get_stock_details(self, stock_code):
-    	stock_details = {}
-    	stock_dict = self.get_stock_quote(stock_code)
-    	stock_details['price'] = stock_dict['lastPrice']
-    	stock_details['52_week_high'] = stock_dict['high52']
-    	stock_details['52_week_low'] = stock_dict['low52']
+        stock_dict = self.get_stock_quote(stock_code)
+        self.stock_details[stock_code]['price'] = stock_dict['lastPrice']
+        self.stock_details[stock_code]['52_week_high'] = stock_dict['high52']
+        self.stock_details[stock_code]['52_week_low'] = stock_dict['low52']
 
 
     def get_all_codes(self):
@@ -26,12 +24,31 @@ class StockReader(object):
     	return all_stock_codes
 
     def get_stock_details_list(self):
+        for stock_code in self.stock_details.keys():
+            self.get_stock_details(stock_code)
         pprint(self.stock_details)
 
- 
+    def total(self):
+        total_bought_price = 0
+        total_price = 0
+        total_high = 0
+        total_low = 0
+        for value in self.stock_details.values():
+
+            total_bought_price += float(value[' brought_price']) * int(value[' quantity '])
+            total_price += float(value['price']) * int(value[' quantity '])
+            total_high += float(value['52_week_high']) * int(value[' quantity '])
+            total_low += float(value['52_week_low']) * int(value[' quantity '])
+
+        print "Total brought price %s " % total_bought_price
+        print "Todays total price %s" % total_price
+        print "Total high price %s" % total_high
+        print "Total low  price %s" % total_low
+
 
 if __name__=="__main__":
-	stock = StockReader()
-	# all_codes = stock.get_all_codes()
-	# pprint(all_codes)
-	stock.get_stock_details_list()
+    stock = StockReader()
+    # all_codes = stock.get_all_codes()
+    # pprint(all_codes)
+    stock.get_stock_details_list()
+    stock.total()
--- utils.py
+++ utils.py
@@ -2,11 +2,15 @@
 import csv
 
 def csv_to_dict():
-	dict_details = {}
-	reader = csv.DictReader(open('stock_list.csv', 'rb'), delimiter='|')
-	for line in reader:
-		keys = line.keys()
-		vals = line.values()
-		dict_details[vals[0]] = {keys[1]: vals[1], keys[2]: vals[2], keys[3]: vals[3]}
+    '''
 
-	return dict_details
+    This code converts csv to dict
+
+    '''
+    dict_details = {}
+    reader = csv.DictReader(open('stock_list.csv', 'rb'), delimiter='|')
+    for line in reader:
+        keys = line.keys()
+        vals = line.values()
+        dict_details[vals[0]] = {keys[1]: vals[1], keys[2]: vals[2], keys[3]: vals[3]}
+    return dict_details
