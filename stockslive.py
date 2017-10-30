bb03a69c7f46c65048a0af59804001e25aed540e
abhijit <abi.vernekar>
2017-10-30 22:02:40 +0530

    Completed stock report as a csv file

--- stockslive.py
+++ stockslive.py
@@ -1,6 +1,7 @@
 from nsetools import Nse
 from pprint import pprint
 from utils import csv_to_dict
+import csv
 
 
 class StockReader(object):
@@ -20,6 +21,7 @@ class StockReader(object):
         self.stock_details[stock_code]['price'] = stock_dict['lastPrice']
         self.stock_details[stock_code]['52_week_high'] = stock_dict['high52']
         self.stock_details[stock_code]['52_week_low'] = stock_dict['low52']
+        self.stock_details[stock_code]['value'] = float(stock_dict['lastPrice']) * int(self.stock_details[stock_code][' quantity '])
 
 
     def get_all_codes(self):
@@ -29,7 +31,23 @@ class StockReader(object):
     def get_stock_details_list(self):
         for stock_code in self.stock_details.keys():
             self.get_stock_details(stock_code)
-        pprint(self.stock_details)
+        return (self.stock_details)
+
+    def csv_files(self):
+        stocks = self.get_stock_details_list()
+        stocks = stocks.values()
+        keys = stocks[0].keys()
+        with open('new_file.csv','wb') as csv_files:
+            dict_writer = csv.DictWriter(csv_files, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(stocks)
+
+        with open('new_file.csv', 'a') as file:
+            writer = csv.writer(file, delimiter=',')
+            total = self.total()
+            total.insert(0, 'Total')
+            writer.writerow(total)
+
 
     def total(self):
         total_bought_price = 0
@@ -48,10 +66,10 @@ class StockReader(object):
         print "Total high price %s" % total_high
         print "Total low  price %s" % total_low
 
+        return [total_high, 'N/A', total_price, 'N/A', total_low, total_bought_price]
 
 if __name__=="__main__":
     stock = StockReader()
     # all_codes = stock.get_all_codes()
     # pprint(all_codes)
-    stock.get_stock_details_list()
-    stock.total()
+    stock.csv_files()
